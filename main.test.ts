cb1d0d7270eade26d6e309dec3bc6103648e05c5
Leonard Crestez <cdleonard@gmail.com>
2022-10-19 19:01:16 +0300

    main.test.ts: prettier
    Signed-off-by: Leonard Crestez <cdleonard@gmail.com>

--- main.test.ts
+++ main.test.ts
@@ -1,42 +1,42 @@
-import { expect, test } from '@jest/globals';
+import { expect, test } from "@jest/globals";
 const JsonURL = require("@jsonurl/jsonurl");
-const fs = require('fs');
+const fs = require("fs");
 
-let path = './test-data/test_data.json'
+let path = "./test-data/test_data.json";
 
 function main() {
-    if (!fs.existsSync(path)) {
-        throw `missing ${path}`
-    }
-    let data_file_text = fs.readFileSync(path, { encoding: "utf8" });
-    let data = JSON.parse(data_file_text)
+  if (!fs.existsSync(path)) {
+    throw `missing ${path}`;
+  }
+  let data_file_text = fs.readFileSync(path, { encoding: "utf8" });
+  let data = JSON.parse(data_file_text);
 
-    for (const [key, val] of Object.entries(data)) {
-        if (key == "$schema") {
-            continue;
-        }
-        test(key, function () {
-            let item = data[key];
-            let type = item["type"];
-            if (type == "roundtrip" || type === undefined) {
-                let text = item.text
-                let data = item.data
-                expect(JsonURL.parse(text)).toEqual(data)
-                expect(JsonURL.stringify(data)).toEqual(text)
-            } else if (type == "fail") {
-                let text = item.text
-                expect(() => JsonURL.parse(text)).toThrow();
-            } else if (type == "load") {
-                let text = item.text
-                let data = item.data
-                expect(JsonURL.parse(text)).toEqual(data)
-            } else {
-                throw `bad test type ${type}`
-            }
-        });
+  for (const [key, val] of Object.entries(data)) {
+    if (key == "$schema") {
+      continue;
     }
+    test(key, function () {
+      let item = data[key];
+      let type = item["type"];
+      if (type == "roundtrip" || type === undefined) {
+        let text = item.text;
+        let data = item.data;
+        expect(JsonURL.parse(text)).toEqual(data);
+        expect(JsonURL.stringify(data)).toEqual(text);
+      } else if (type == "fail") {
+        let text = item.text;
+        expect(() => JsonURL.parse(text)).toThrow();
+      } else if (type == "load") {
+        let text = item.text;
+        let data = item.data;
+        expect(JsonURL.parse(text)).toEqual(data);
+      } else {
+        throw `bad test type ${type}`;
+      }
+    });
+  }
 }
 
 main();
 
-export { }
+export {};
