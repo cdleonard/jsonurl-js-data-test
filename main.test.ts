1bdb571d62141406e3ab77bbdfb19da43eb7fac3
Leonard Crestez <cdleonard@gmail.com>
2022-10-19 18:28:03 +0300

    main.test.ts: Adjust to new format
    Signed-off-by: Leonard Crestez <cdleonard@gmail.com>

--- main.test.ts
+++ main.test.ts
@@ -11,9 +11,12 @@ function main() {
     let data_file_text = fs.readFileSync(path, { encoding: "utf8" });
     let data = JSON.parse(data_file_text)
 
-    for (let index = 0; index < data.length; index++) {
-        test(index.toString(), function () {
-            let item = data[index]
+    for (const [key, val] of Object.entries(data)) {
+        if (key == "$schema") {
+            continue;
+        }
+        test(key, function () {
+            let item = data[key];
             let type = item["type"];
             if (type == "roundtrip" || type === undefined) {
                 let text = item.text
@@ -23,8 +26,12 @@ function main() {
             } else if (type == "fail") {
                 let text = item.text
                 expect(() => JsonURL.parse(text)).toThrow();
+            } else if (type == "load") {
+                let text = item.text
+                let data = item.data
+                expect(JsonURL.parse(text)).toEqual(data)
             } else {
-                throw `bad type ${type}`
+                throw `bad test type ${type}`
             }
         });
     }
